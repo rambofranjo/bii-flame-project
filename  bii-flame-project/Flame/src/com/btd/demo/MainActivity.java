c0af1eddb017af27d75ac062bef195cad53a9002
markovicb@gmx.at <markovicb@gmx.at@6c0f8cec-1942-cd15-fe6f-3a68a88334fc>
2013-01-13 17:07:33 +0000

    facebook login

---  bii-flame-project/Flame/src/com/btd/demo/MainActivity.java
+++  bii-flame-project/Flame/src/com/btd/demo/MainActivity.java
@@ -21,7 +21,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -31,12 +34,19 @@ import com.facebook.model.*;
 
 public class MainActivity extends FragmentActivity implements SensorEventListener, OnClickListener{
 	
+	private UiLifecycleHelper uiHelper;
+	
+	private boolean isResumed = false;
+	private static final int SPLASH = 0;
+	private static final int SELECTION = 1;
+	private static final int FRAGMENT_COUNT = SELECTION +1;
+
+	private Fragment[] fragments = new Fragment[FRAGMENT_COUNT];
+	
 	private SensorManager mSensorManager;
-	private Sensor mLight;
-	private Button buttonRead = null;  
+	private Sensor mLight; 
 	private float sensorReading = 0;
 
-	private Button buttonLight = null;
 	UsbAccessory mAccessory;
 	ParcelFileDescriptor mFileDescriptor;
 	FileInputStream mInputStream;
@@ -97,6 +107,10 @@ public class MainActivity extends FragmentActivity implements SensorEventListene
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+	    uiHelper = new UiLifecycleHelper(this, callback);
+	    uiHelper.onCreate(savedInstanceState);
+		
 		// here you set the layout resource that is defining the UI of the MainActivity
 		setContentView(R.layout.main);
 		
@@ -105,11 +119,38 @@ public class MainActivity extends FragmentActivity implements SensorEventListene
 		
 		IntentFilter filter = new IntentFilter("com.google.android.BeyondTheDesktop.action.USB_PERMISSION");
 		filter.addAction(UsbManager.ACTION_USB_ACCESSORY_DETACHED);
-		registerReceiver(mUsbReceiver, filter);	
-
+		registerReceiver(mUsbReceiver, filter);
 		
+		
+		
+		FragmentManager fm = getSupportFragmentManager();
+	    fragments[SPLASH] = fm.findFragmentById(R.id.splashFragment);
+	    fragments[SELECTION] = fm.findFragmentById(R.id.selectionFragment);
+
+	    FragmentTransaction transaction = fm.beginTransaction();
+	    for(int i = 0; i < fragments.length; i++) {
+	        transaction.hide(fragments[i]);
+	    }
+	    transaction.commit();
 		
 	}
+	
+	private void showFragment(int fragmentIndex, boolean addToBackStack) {
+	    FragmentManager fm = getSupportFragmentManager();
+	    FragmentTransaction transaction = fm.beginTransaction();
+	    for (int i = 0; i < fragments.length; i++) {
+	        if (i == fragmentIndex) {
+	            transaction.show(fragments[i]);
+	        } else {
+	            transaction.hide(fragments[i]);
+	        }
+	    }
+	    if (addToBackStack) {
+	        transaction.addToBackStack(null);
+	    }
+	    transaction.commit();
+	}
+	
 	
 	@Override
 	protected void onResume() {
@@ -121,14 +162,68 @@ public class MainActivity extends FragmentActivity implements SensorEventListene
 		// add listener. The listener will be MainActivity (this) class
 		mSensorManager.registerListener(this, mLight, SensorManager.SENSOR_DELAY_NORMAL);
 		super.onResume();
+	    uiHelper.onResume();
+	    isResumed = true;
 	}
 	
 	@Override
 	protected void onPause() {	
 		mSensorManager.unregisterListener(this);
 		super.onPause();
+	    uiHelper.onPause();
+	    isResumed = false;
+	    
 	    Log.d("MainActivity","onPause"); 
 	}
+	
+	private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+	    // Only make changes if the activity is visible
+	    if (isResumed) {
+	        FragmentManager manager = getSupportFragmentManager();
+	        // Get the number of entries in the back stack
+	        int backStackSize = manager.getBackStackEntryCount();
+	        // Clear the back stack
+	        for (int i = 0; i < backStackSize; i++) {
+	            manager.popBackStack();
+	        }
+	        if (state.isOpened()) {
+	            // If the session state is open:
+	            // Show the authenticated fragment
+	            showFragment(SELECTION, false);
+	        } else if (state.isClosed()) {
+	            // If the session state is closed:
+	            // Show the login fragment
+	            showFragment(SPLASH, false);
+	        }
+	    }
+	}
+	
+	@Override
+	protected void onResumeFragments() {
+	    super.onResumeFragments();
+	    Session session = Session.getActiveSession();
+
+	    if (session != null && session.isOpened()) {
+	        // if the session is already open,
+	        // try to show the selection fragment
+	        showFragment(SELECTION, false);
+	    } else {
+	        // otherwise present the splash screen
+	        // and ask the user to login.
+	        showFragment(SPLASH, false);
+	    }
+	}
+	
+	
+	private Session.StatusCallback callback = 
+	    new Session.StatusCallback() {
+	    @Override
+	    public void call(Session session, 
+	            SessionState state, Exception exception) {
+	        onSessionStateChange(session, state, exception);
+	    }
+	};
+	
 	
 	@Override
 	public void onAccuracyChanged(Sensor arg0, int arg1) {
@@ -158,6 +253,24 @@ public class MainActivity extends FragmentActivity implements SensorEventListene
 			break;
 			**/
 		}
+	}
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    super.onActivityResult(requestCode, resultCode, data);
+	    uiHelper.onActivityResult(requestCode, resultCode, data);
+	}
+
+	@Override
+	public void onDestroy() {
+	    super.onDestroy();
+	    uiHelper.onDestroy();
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+	    super.onSaveInstanceState(outState);
+	    uiHelper.onSaveInstanceState(outState);
 	}
 	
 	
